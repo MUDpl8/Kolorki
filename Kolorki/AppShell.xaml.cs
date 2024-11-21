73ab67ba177170e57b968481becad81c6f449825
PKruk 3TB <PKruk 3TB@DESKTOP-LR2HC22>
2024-11-21 09:18:00 +0100

    Dodaj pliki projektów.

---
+++ Kolorki/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Kolorki
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Kolorki/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Controls.Compatibility.Platform;
+
+namespace Kolorki
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            Color color = Color.FromRgb(SliderR.Value, SliderG.Value, SliderB.Value);
+            kwadrat.Fill = new SolidColorBrush(color);
+        }
+    }
+
+}
